d30fc6d8a303d76a1666149ca58c7e8f58b24cf9
Allanella <allanbaliddawa@gmail.com>
2026-03-28 19:39:41 +0300

    fix: destructure copyQuarterData import in quarterCopy routes

--- backend/src/routes/quarterCopy.routes.js
+++ backend/src/routes/quarterCopy.routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const quarterCopyController = require('../controllers/quarterCopyController');
+const { copyQuarterData } = require('../controllers/quarterCopyController');
 const authenticate = require('../middleware/auth');
 
 // Copy classes and members from one quarter to another
-router.post('/copy', authenticate, quarterCopyController.copyQuarterData);
+router.post('/copy', authenticate, copyQuarterData);
 
 module.exports = router;
