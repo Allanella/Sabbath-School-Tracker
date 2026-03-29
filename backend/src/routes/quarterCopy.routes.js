52224922aeceb82f8e258f7be36159afbe189b16
Allanella <allanbaliddawa@gmail.com>
2026-03-29 13:35:00 +0300

    remove broken quarterCopy routes file - now registered directly in server.js

--- backend/src/routes/quarterCopy.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { copyQuarterData } = require('../controllers/quarterCopyController');
-const authenticate = require('../middleware/auth');
-
-// Copy classes and members from one quarter to another
-router.post('/copy', authenticate, copyQuarterData);
-
-module.exports = router;
