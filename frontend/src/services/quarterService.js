a4dfb66d99bfc852b5752959f81c3d722861824b
Allanella <allanbaliddawa@gmail.com>
2026-04-06 14:20:30 +0300

    fix: correct setActive endpoint to use POST body instead of URL parameter

--- frontend/src/services/quarterService.js
+++ frontend/src/services/quarterService.js
@@ -1,43 +1,25 @@
 import api from './api';
 
 const quarterService = {
-  getAll: async () => {
-    const response = await api.get('/quarters');
-    return response.data;
-  },
-
-  getById: async (id) => {
-    const response = await api.get(`/quarters/${id}`);
-    return response.data;
-  },
-
-  create: async (quarterData) => {
-    const response = await api.post('/quarters', quarterData);
-    return response.data;
-  },
-
-  update: async (id, quarterData) => {
-    const response = await api.put(`/quarters/${id}`, quarterData);
-    return response.data;
-  },
-
-  delete: async (id) => {
-    const response = await api.delete(`/quarters/${id}`);
-    return response.data;
-  },
-
-  setActive: async (id) => {
-    const response = await api.post(`/quarters/${id}/set-active`);
-    return response.data;
-  },
-
-  copyFromPreviousQuarter: async (sourceQuarterId, targetQuarterId) => {
-    const response = await api.post('/quarters/copy', {
+  getAll: () => api.get('/quarters'),
+  
+  getActive: () => api.get('/quarters/active'),
+  
+  getById: (id) => api.get(`/quarters/${id}`),
+  
+  create: (data) => api.post('/quarters', data),
+  
+  update: (id, data) => api.put(`/quarters/${id}`, data),
+  
+  delete: (id) => api.delete(`/quarters/${id}`),
+  
+  setActive: (id) => api.post('/quarters/set-active', { quarter_id: id }),
+  
+  copyFromPreviousQuarter: (sourceQuarterId, targetQuarterId) => 
+    api.post('/quarters/copy', {
       source_quarter_id: sourceQuarterId,
       target_quarter_id: targetQuarterId
-    });
-    return response.data;
-  }
+    })
 };
 
 export default quarterService;
